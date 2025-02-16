b727b4096e79a80f2b879d92c7ba5cc8ba74c860
Maggie <136146330+themaggieluong@users.noreply.github.com>
2025-02-16 02:36:45 -0500

    Delete react directory

--- react/src/api/CategoryApi.js
+++
@@ -1,7 +0,0 @@
-import { utilFetchWrapper } from "../services/utilFetchWrapper";
-const fetchWrapper = utilFetchWrapper();
-const baseUrl = '/category';
-
-export const getCategories = () => {
-    return fetchWrapper.get(baseUrl);
-}
